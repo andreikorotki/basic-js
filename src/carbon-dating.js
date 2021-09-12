bef3ff7b2507bbc9c3a366eeed68be026405ea2c
Andrei Karotki <Andreikorotki@gmail.com>
2021-09-12 15:50:55 +0300

    feat: three tasks ready

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+//Math.log2()
+const parsed = parseFloat(sampleActivity);
+if (typeof sampleActivity !== 'string') { return false; }
+else if (isNaN(parsed)) { return false; }
+else if (parsed < 1 || parsed > 15) { return false; } 
+else {
+  return Math.ceil(Math.log(MODERN_ACTIVITY/parsed) / (0.693/HALF_LIFE_PERIOD));
+}
 }
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+let letterList = [];
+  if (! Array.isArray(members)) {return false;}
+  for (let index = 0; index < members.length; index++) {
+    let element = members[index];
+    if (typeof element === 'string') {
+      element = element.trim();
+      letterList.push(element[0].toUpperCase())
+    }
+  }
+
+  return letterList.sort(function(a, b) {
+    var la = a.toLowerCase(), 
+    lb = b.toLowerCase();
+    return la > lb ? 1 : la < lb ? -1 : 0;
+  }).join('');
 }
+
+//console.log(typeof ['Matt', 'Ann', 'Dmitry', 'Max']);
